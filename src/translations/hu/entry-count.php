0961ccb31d79ac3c239e5fccffe87d61f64e795c
Peter Varga <info@vargapeter.com>
2019-01-12 22:50:05 +0100

    HU translation

---
+++ src/translations/hu/entry-count.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Entry Count - Hungarian Translation
+*/
+return [
+    'Count' => 'Számláló',
+    'Entry count reset.' => 'Számláló visszaállítása',
+    'Ignore logged in users' => 'Bejelentkezett felhasználók figyelmen kívül hagyása', 
+    'Only increment counts if users are not logged in'' => 'Csak abban az esetben növekedik a megtekintések száma, ha a felhasználó nincs bejelentkezve',
+    'Ignore IP addresses' => 'IP címek figyelmen kívül hagyása',
+    'Add one IP address per line to ignore them when incrementing counts' => 'A hozzáadott IP címekről (mindegyik új sorban) érkező kérések esetén nem fog növekedni a megtekintések száma',
+    'Entry Count' => 'Bejegyzés Számláló',
+    'Entry' => 'Bejegyzés',
+    'URL' => 'URL',
+    'First Count' => 'Első megtekintés',
+    'Last Count' => 'Utolsó megtekintés',
+    'No entries counted' => 'Még nincsenek megtekintések'
+];
