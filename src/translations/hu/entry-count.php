f7ea41221d540b48b05044e6d89ed9fe4987dcf5
Peter Varga <admin@vargapeter.com>
2019-01-12 22:50:24 +0100

    typo

--- src/translations/hu/entry-count.php
+++ src/translations/hu/entry-count.php
@@ -6,7 +6,7 @@ return [
     'Count' => 'Számláló',
     'Entry count reset.' => 'Számláló visszaállítása',
     'Ignore logged in users' => 'Bejelentkezett felhasználók figyelmen kívül hagyása', 
-    'Only increment counts if users are not logged in'' => 'Csak abban az esetben növekedik a megtekintések száma, ha a felhasználó nincs bejelentkezve',
+    'Only increment counts if users are not logged in' => 'Csak abban az esetben növekedik a megtekintések száma, ha a felhasználó nincs bejelentkezve',
     'Ignore IP addresses' => 'IP címek figyelmen kívül hagyása',
     'Add one IP address per line to ignore them when incrementing counts' => 'A hozzáadott IP címekről (mindegyik új sorban) érkező kérések esetén nem fog növekedni a megtekintések száma',
     'Entry Count' => 'Bejegyzés Számláló',
